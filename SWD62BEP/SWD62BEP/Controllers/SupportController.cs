2175d0db46f1e1c3223f2f93b1086c4e1da48e3c
Joshua <joshua.portelli.c10270@mcast.edu.mt>
2020-10-26 11:20:50 +0100

    Lesson 3 - Setting up the basis for our clean architecture

---
+++ SWD62BEP/SWD62BEP/Controllers/SupportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SWD62BEP.Controllers
+{
+    public class SupportController : Controller
+    {
+        [HttpGet]
+        public IActionResult Contact() //this will be used to load the page
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(string email, string query) //this will be used to handle the form submission
+        {
+            if (string.IsNullOrEmpty(query))
+                ViewData["warning"] = "Type in some question";
+            else //inform the responsible staff
+                ViewData["feedback"] = "Thank you for getting in touch with us. We will answer back shortly.";
+
+            return View();
+        }
+    }
+}
