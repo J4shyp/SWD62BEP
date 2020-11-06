6b0e270df3210971739953349eb6daa2fa10edc1
Joshua <joshua.portelli.c10270@mcast.edu.mt>
2020-11-06 13:37:34 +0100

    06/11/2020

--- SWD62BEP/SWD62BEP/Startup.cs
+++ SWD62BEP/SWD62BEP/Startup.cs
@@ -12,6 +12,7 @@ using SWD62BEP.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShoppingCart.Data.Context;
 
 namespace SWD62BEP
 {
@@ -30,6 +31,11 @@ namespace SWD62BEP
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<ShoppingCartDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
---
+++ SWD62BEP/ShoppingCart.Domain/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ShoppingCart.Domain.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public String Description { get; set; }
+        [Required]
+
+        public Category Category { get; set; }
+    }
+}
